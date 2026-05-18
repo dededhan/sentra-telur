8a51f5ccde215bc059b4319658129e95a1d2e10e
dede1106 <114045101+dededhan@users.noreply.github.com>
2026-05-18 14:31:54 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // <--- Tambahkan baris ini
   experimental: {
     serverActions: {
       bodySizeLimit: '5mb',
